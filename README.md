5d381daca75952d456bbece11b46c6d683e56f30
s1ntaxe770r <cyberdev@protonmail.com>
2020-04-26 23:38:10 +0100

    initial push

---
+++ README.md
@@ -0,0 +1,15 @@
+# Usage 
+
+
+## make sure you have virtualenv installed if not ` pip3 install virtualenv`
+
+- clone the repo
+- navigate to the project folder and activate the virtual enviroment using:
+	- `source env/bin/activate` for unix based systems  ` \env\scripts\activate` for windows
+- run ` pip install -r requirements.txt ` to install dependencies
+
+- run the app ` python app.py `
+
+- navigate to `127.0.0.1:5000` to view the app
+
+
--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import os
 from flask import Flask, url_for, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from  time import  strftime
+from time import strftime
+
+
+
 project_dir = os.path.dirname(os.path.abspath(__file__))
 db_file = "sqlite:///{}".format(os.path.join(project_dir, "todo.db"))
 
@@ -13,57 +16,39 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
 class Todo(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	task = db.Column(db.String(80),nullable=False)
-	created = db.Column(db.DateTime,nullable=False,default=datetime.today())   
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(80), nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.today())
+
+    def __repr__(self):
+        return '<Todo: {}>'.format(self.task)
 
 
-	def __repr__(self):
-		return '<Todo: {}>'.format(self.task)
-
-		
-
-# class Todo(db.Model):
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	task = db.Column(db.String(80),nullable=False)
-#     created = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)     
-
-# 	def __repr__(self):
-# 		return '<Todo: {}>'.format(self.task)
-
-
-
-@app.route('/',methods=["GET","POST"])
+@app.route('/', methods=["GET", "POST"])
 def index():
-	if request.method == "POST":
-		if request.form != " ":
-			todo = Todo(task=request.form.get("task"))
-			db.session.add(todo)
-			db.session.commit()
-	todos = Todo.query.all()
-	return render_template('index.html',todos=todos)
+    if request.method == "POST":
+        if request.form != " ":
+            todo = Todo(task=request.form.get("task"))
+            db.session.add(todo)
+            db.session.commit()
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
 
 
-
-@app.route("/delete",methods=["POST"])
+@app.route("/delete", methods=["POST"])
 def delete():
-	todo = request.form.get("task")
-	task = Todo.query.filter_by(task=todo).first()
-	db.session.delete(task)
-	db.session.commit()
-	return redirect("/")
+    todo = request.form.get("task")
+    task = Todo.query.filter_by(task=todo).first()
+    db.session.delete(task)
+    db.session.commit()
+    return redirect("/")
+
+
+@app.route('/social',methods=['GET'])
+def social():
+	return redirect('https://github.com/s1ntaxe770r')
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',debug=True)
-
-
-
-
-
-
-
-
-
+    app.run()
