2424c14b2937292a9838ccabb41d166d06b283ce
s1ntaxe770r <cyberdev@protonmail.com>
2020-04-26 06:52:44 +0100

    last working build

--- app.py
+++ app.py
@@ -1,27 +1,38 @@
 import os
-from flask import Flask,url_for,render_template,request
-from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask, url_for, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+from  time import  strftime
 project_dir = os.path.dirname(os.path.abspath(__file__))
-db_file = "sqlite:///{}".format(os.path.join(project_dir,"todo.db"))
+db_file = "sqlite:///{}".format(os.path.join(project_dir, "todo.db"))
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = db_file
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 
 class Todo(db.Model):
-	id = db.Column(db.Integer,primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
 	task = db.Column(db.String(80),nullable=False)
-	created =  db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+	created = db.Column(db.DateTime,nullable=False,default=datetime.today())   
 
 
 	def __repr__(self):
 		return '<Todo: {}>'.format(self.task)
+
+		
+
+# class Todo(db.Model):
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	task = db.Column(db.String(80),nullable=False)
+#     created = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)     
+
+# 	def __repr__(self):
+# 		return '<Todo: {}>'.format(self.task)
+
 
 
 @app.route('/',methods=["GET","POST"])
@@ -32,12 +43,21 @@ def index():
 			db.session.add(todo)
 			db.session.commit()
 	todos = Todo.query.all()
-	print(todos)
 	return render_template('index.html',todos=todos)
 
 
+
+@app.route("/delete",methods=["POST"])
+def delete():
+	todo = request.form.get("task")
+	task = Todo.query.filter_by(task=todo).first()
+	db.session.delete(task)
+	db.session.commit()
+	return redirect("/")
+
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0',debug=True)
 
 
 
